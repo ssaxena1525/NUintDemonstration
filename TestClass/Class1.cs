ce1b538b2af87db7bf1c08a2bb0e666331eaf115
ssaxena1525 <ssaxena1525@conestogac.on.ca>
2024-10-15 20:33:17 -0400

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitDemonstration;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace TestClass
+{
+    [TestFixture]
+    //Shivang Saxena
+    public class TriangleTest
+    {
+        [Test]
+
+        public void ValidTriangle_Input60and60and60_OutputVaildTriangle()
+        {
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            string expected = "The triangle is valid.";
+
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            ClassicAssert.AreEqual(expected, actual);
+
+        }
+    }
+}
